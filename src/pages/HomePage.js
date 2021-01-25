f03e11d376d18f3a4d8901ef954f9aa3ec3cca7e
Talha-35 <tlh.34.bd@gmail.com>
2021-01-25 15:51:32 +0100

    created Home page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+// import Typography from "@material-ui/core/Typography";
+import CardList from '../components/CardList'
+
+const HomePage = () => {
+
+
+  return (
+    <Container>
+      <CardList />     
+    </Container>
+  );
+};
+export default HomePage;
