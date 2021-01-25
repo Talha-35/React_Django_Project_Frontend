9a053f257c8821795a1c8bbeb3317e1859c9a5c5
Talha-35 <tlh.34.bd@gmail.com>
2021-01-25 15:52:08 +0100

    edited App js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import DetailPage from './pages/DetailPage';
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
+        {/* <HomePage/> */}
+        <DetailPage/>
     </div>
   );
 }
