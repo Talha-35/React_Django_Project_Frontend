f7e9d35dcc738391ee856a2398800baf39a95e63
Talha-35 <tlh.34.bd@gmail.com>
2021-01-26 17:03:04 +0100

    updated some styles

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,22 +1,17 @@
 import React from "react";
 
-import {
-  fade,
-  ThemeProvider,
-  withStyles,
-  createMuiTheme,
-} from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 
 import TextField from "@material-ui/core/TextField";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import LocalMallIcon from "@material-ui/icons/LocalMall";
-import useMediaQuery from '@material-ui/core/useMediaQuery';
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import CKEditor from "ckeditor4-react";
 
 const CssTextField = withStyles({
   root: {
@@ -45,72 +40,50 @@ const useStyles = makeStyles((theme) => ({
   root: {
     height: "100vh",
   },
-  image: {
-    backgroundImage: "url(https://picsum.photos/640/480)",
-    backgroundRepeat: "no-repeat",
-    backgroundColor:
-      theme.palette.type === "light"
-        ? theme.palette.grey[50]
-        : theme.palette.grey[900],
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  },
   paper: {
     margin: theme.spacing(8, 4),
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
-    // backgroundColor: "yellow",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.primary.main,
   },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-    marginTop : "4rem",
-
-  },
-  linkWrapper: {
-    textAlign: "center",
-    margin: theme.spacing(1.5),
-  },
   form: {
-    marginTop : "3rem",
+    marginTop: "3rem",
     alignItems: "center",
-    // backgroundColor: "green",
-    paddingLeft : '20%',
-    paddingRight : '5%',
+    paddingLeft: "20%",
+    paddingRight: "5%",
   },
   form2: {
-    marginTop : "3rem",
+    marginTop: "3rem",
     alignItems: "center",
   },
-  margin : {
-    margin : 2,  
-    marginTop : 13,  
+  margin: {
+    margin: 2,
+    marginTop: 13,
   },
-  address : {
-    marginTop : 13,  
-    margin : 2,    
-    width : "80.5%",   
+  address: {
+    marginTop: 13,
+    margin: 2,
+    width: "80.5%",
   },
-  bio : {
-    margin : 2,    
-    // width : "80.5%",   
-    marginTop : 13,  
+  bio: {
+    margin: 2,
+    // width : "80.5%",
+    marginTop: 13,
   },
-  button : {
-    marginTop : 13,  
-    width : "80.7%",       
-},
+  button: {
+    marginTop: 13,
+    width: "80.7%",
+  },
 }));
 
 const ProfilePage = () => {
-  
   const classes = useStyles();
-  const matches = useMediaQuery('(min-width:750px)');
+  const matches = useMediaQuery("(min-width:750px)");
 
   return (
     <Grid container component="main" className={classes.root}>
@@ -122,68 +95,82 @@ const ProfilePage = () => {
           <Typography component="h1" variant="h5">
             Profile
           </Typography>
-          <form className={matches ? classes.form : classes.form2} >
-              
+          <form className={matches ? classes.form : classes.form2}>
             <CssTextField
               className={classes.margin}
-              style={{width : matches ? "40%" : "100%" }}
+              style={{ width: matches ? "40%" : "100%" }}
               variant="outlined"
               id="firstname"
               name="firstname"
               label="First Name"
-              />
+            />
             <CssTextField
               className={classes.margin}
-              style={{width : matches ? "40%" : "100%" }}
+              style={{ width: matches ? "40%" : "100%" }}
               id="last_name"
               name="last_name"
               label="Last Name"
               variant="outlined"
-              />
+            />
 
             <CssTextField
               className={classes.margin}
-              style={{width : matches ? "40%" : "100%" }}
+              style={{ width: matches ? "40%" : "100%" }}
               variant="outlined"
               id="country"
               name="country"
               label="Country"
-              />
+            />
 
             <CssTextField
               className={classes.margin}
-              style={{width : matches ? "40%" : "100%" }}
+              style={{ width: matches ? "40%" : "100%" }}
               variant="outlined"
               id="phone"
               name="phone"
               label="Phone"
-              />
+            />
             <CssTextField
               className={classes.address}
-              style={{width : matches ? "80.7%" : "100%" }}
+              style={{ width: matches ? "80.7%" : "100%" }}
               variant="outlined"
               id="adress"
               name="address"
               label="Address"
-              />
-
-            <CssTextField
+            />
+            {matches ? (
+              <div
+                className="App"
+                style={{ marginTop: 20, width: matches ? "80.5%" : "100%" }}
+              >
+                <CKEditor
+                // data="<p>Hello from CKEditor 4!</p>"
+                />
+              </div>
+            ) : (
+              <CssTextField
               className={classes.bio}
-              style={{width : matches ? "80.7%" : "100%" }}
+              style={{ width: matches ? "80.7%" : "100%" }}
               variant="outlined"
+              placeholder = 'Biography'
               id="bio"
               name="bio"
               label="Biografy"
-              />
+            />
+            )}
+
+            
 
             <Button
               color="primary"
-              style={{width : matches ? "80.7%" : "100%" , marginTop : matches ? null : 30 }}
+              style={{
+                width: matches ? "80.7%" : "100%",
+                marginTop: matches ? null : 30,
+              }}
               variant="contained"
               fullWidth
               type="submit"
               className={classes.button}
-              
             >
               Update
             </Button>
