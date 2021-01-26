589a69ec9e620ff90326f08821f81cb3700cecb2
Talha-35 <tlh.34.bd@gmail.com>
2021-01-26 18:10:42 +0100

    added axios await

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Container from "@material-ui/core/Container";
 // import Typography from "@material-ui/core/Typography";
-import CardList from '../components/CardList'
+import CardList from '../components/CardList';
+import axios from 'axios';
 
 const HomePage = () => {
 
+  const fetchData = async () => {
+    const result= await axios.get(
+      'https://rd-restful-blog.herokuapp.com/list/',
+    );
+    console.log("result.data :", result.data)
+    return (result.data)
+}
+
+useEffect(() =>{
+  const response = fetchData()
+  console.log("response :  ", response)
+
+})
+  
 
   return (
     <Container>
