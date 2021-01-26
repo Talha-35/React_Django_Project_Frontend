b4abcd59b572178ccac62f620c53abb010cb5f7f
Talha-35 <tlh.34.bd@gmail.com>
2021-01-26 17:03:17 +0100

    updated some styles

--- src/pages/UpdatePage.js
+++ src/pages/UpdatePage.js
@@ -1,17 +1,11 @@
 import React from "react";
 
-import {
-  fade,
-  ThemeProvider,
-  withStyles,
-  createMuiTheme,
-} from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 
 import TextField from "@material-ui/core/TextField";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
@@ -19,7 +13,8 @@ import LocalMallIcon from "@material-ui/icons/LocalMall";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
-import Select from '@material-ui/core/Select';
+import Select from "@material-ui/core/Select";
+import CKEditor from 'ckeditor4-react';
 
 const CssTextField = withStyles({
   root: {
@@ -48,44 +43,28 @@ const useStyles = makeStyles((theme) => ({
   root: {
     height: "100vh",
   },
-  image: {
-    backgroundImage: "url(https://picsum.photos/640/480)",
-    backgroundRepeat: "no-repeat",
-    backgroundColor:
-      theme.palette.type === "light"
-        ? theme.palette.grey[50]
-        : theme.palette.grey[900],
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  },
   paper: {
     margin: theme.spacing(8, 4),
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
-    // backgroundColor: "yellow",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.primary.main,
   },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-    marginTop: "4rem",
-  },
-  linkWrapper: {
-    textAlign: "center",
-    margin: theme.spacing(1.5),
-  },
+
   form: {
     marginTop: "3rem",
     alignItems: "center",
+    width : '80%',
     paddingLeft: "10%",
   },
   form2: {
     marginTop: "3rem",
     alignItems: "center",
+    width : '100%',
   },
   margin: {
     margin: 2,
@@ -95,12 +74,7 @@ const useStyles = makeStyles((theme) => ({
     marginTop: 13,
     margin: 2,
     width: "80.5%",
-  },
-  bio: {
-    margin: 2,
-    // width : "80.5%",
-    marginTop: 13,
-  },
+  },  
   button: {
     marginTop: 13,
     width: "80.7%",
@@ -108,8 +82,8 @@ const useStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(1),
     minWidth: 500,
-    display:'block',
-    marginLeft : -0.1,
+    display: "block",
+    marginLeft: -0.1,
     width: "40%",
     "& label.Mui-focused": {
       color: "green",
@@ -128,13 +102,14 @@ const useStyles = makeStyles((theme) => ({
         borderColor: "green",
       },
     },
-    
   },
 }));
 
 const UpdatePage = () => {
+
   const classes = useStyles();
   const matches = useMediaQuery("(min-width:750px)");
+
   const [state, setState] = React.useState({
     Status: "",
     name: "hai",
@@ -175,9 +150,21 @@ const UpdatePage = () => {
               label="Image URL"
               variant="outlined"
             />
-            <CssTextField
+            {
+              matches
+              ?
+              <div className="App" style={{  marginTop:20,width: matches ? "80.5%" : "100%" }}>
+          
+              <CKEditor
+                  // data="<p>Hello from CKEditor 4!</p>"
+                  
+              />
+          </div>
+              :
+          <CssTextField
+            
               className={classes.margin}
-              style={{ width: matches ? "80.5%" : "100%" }}
+              style={{width: matches ? "80.5%" : "100%" }}
               variant="outlined"
               multiline
               rows={8}
@@ -185,21 +172,24 @@ const UpdatePage = () => {
               name="content"
               label="Content"
             />
+            }
+             
             <FormControl variant="outlined" className={classes.formControl}>
-              <InputLabel htmlFor="outlined-age-native-simple">Age</InputLabel>
+              <InputLabel htmlFor="outlined-age-native-simple">Status</InputLabel>
               <Select
-                native
-                value={state.age}
-                onChange={handleChange}
-                label="Status"
-                inputProps={{
-                  name: "Status",
-                  id: "outlined-age-native-simple",
-                }}
+              native
+              value={state.age}
+              onChange={handleChange}
+              label="Status"
+              inputProps={{
+                  name: 'status',
+                  id: 'outlined-age-native-simple',
+              }}
               >
-                <option aria-label="None" value="" />
-                <option value={10}>Draft</option>
-                <option value={20}>Published</option>
+              <option aria-label="None" value="" />
+              <option value={'d'}>Draft</option>
+              <option value={'p'}>Published</option>
+              
               </Select>
             </FormControl>
             <Button
@@ -213,10 +203,11 @@ const UpdatePage = () => {
               type="submit"
               className={classes.button}
             >
-              Submit
+              Update
             </Button>
           </form>
         </div>
+       
       </Grid>
     </Grid>
   );
