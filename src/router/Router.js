a9b0241bc5ea417c2d1ba2684ad53492d9d99978
Talha-35 <tlh.34.bd@gmail.com>
2021-01-26 10:13:37 +0100

    updated Router

--- src/router/Router.js
+++ src/router/Router.js
@@ -4,16 +4,19 @@ import Navbar from '../components/Navbar';
 import DetailPage from '../pages/DetailPage';
 import HomePage from '../pages/HomePage';
 import RegisterPage from '../pages/RegisterPage';
+import ProfilePage from '../pages/ProfilePage';
+import LoginPage from '../pages/LoginPage';
 
 const AppRouter = () => {
     return (
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/home" component={HomePage} exact />
+          <Route path="/" component={HomePage} exact />
           <Route path="/detail" component={DetailPage} />
           <Route path="/register" component={RegisterPage} />
           <Route path="/login" component={LoginPage} />
+          <Route path="/profile" component={ProfilePage} />
         </Switch>
         {/* <FootBar /> */}
       </Router>
