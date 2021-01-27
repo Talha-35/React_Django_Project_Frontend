67380f6051a39d7cbdfe8fcde440c63eecd5a06c
Talha-35 <tlh.34.bd@gmail.com>
2021-01-27 15:08:11 +0100

    added axios fonksiyon

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState , useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from '@material-ui/core/styles';
 import CardDetail from "../components/CardDetail";
+import { useParams } from "react-router-dom";
+import axios from "axios"
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    overflow : 'hidden'
     // alignItems : 'center'
   },
   paper: {
@@ -17,13 +20,24 @@ const useStyles = makeStyles((theme) => ({
 
 const DetailPage = () => {
   const classes = useStyles();
+  const { slug } = useParams();
+  const [postDetail, setPostDetail] = useState()
+  const fetchData = async () => {
+    const res = await axios.get(`https://rd-restful-blog.herokuapp.com/${slug}/detail`)
+    // const res = await axios.get(`https://blog-backend-ysf.herokuapp.com/${slug}/detail`)
+    setPostDetail(res?.data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
           {/* <BlogCard /> */}
-          <CardDetail />
+          <CardDetail post={postDetail} />
         </Grid>
       </Grid>
     </div>
