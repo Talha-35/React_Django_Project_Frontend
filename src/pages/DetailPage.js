030fb24c29f8ad40271e8ecb8940a52d10edd22e
Talha-35 <tlh.34.bd@gmail.com>
2021-01-30 22:01:24 +0100

    added fetchDataDetail

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,46 +1,52 @@
-import React, {useState, useEffect} from "react";
+import React,{ useEffect, useState } from "react";
 import Grid from "@material-ui/core/Grid";
-import Container from "@material-ui/core/Container";
 import { makeStyles } from '@material-ui/core/styles';
 import CardDetail from "../components/CardDetail";
-import Paper from '@material-ui/core/Paper';
 import { useParams } from "react-router-dom";
-import axios from "axios"
-import MenuComponent from "../components/MenuComponent"
+import MenuComponent from '../components/MenuComponent'
+import { fetchDataDetail } from "../helper/FetchData";
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    overflow:'hidden',
     flexGrow: 1,
-    overflow:"hidden"
+    marginBottom:150
   },
   paper: {
     padding: theme.spacing(2),
     textAlign: "center",
     color: theme.palette.text.secondary,
   },
+ 
 }));
 
 const DetailPage = () => {
-  const classes = useStyles();
   const { slug } = useParams();
+  const classes = useStyles();
+
   const [postDetail, setPostDetail] = useState()
-  const fetchData = async () => {
-    const res = await axios.get(`https://blog-backend-ysf.herokuapp.com/${slug}/detail`)
-    setPostDetail(res?.data)
+
+  fetchDataDetail(slug)
+  .then((data) => { 
+    setPostDetail(data)
+    
+  })
+  .catch((err) => {
+    // toast.error(err.message || " an error occured");
+    console.log(err)      
+  });
     
 
-  }
-
   useEffect(() => {
-    fetchData()
+    fetchDataDetail()
   }, [])
+
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
+        {localStorage.getItem('Token') ? <MenuComponent slug = { slug }/> : null}
         <Grid item xs={12}>
-        {localStorage.getItem('currentUser') === postDetail?.author ? <MenuComponent slug={slug}/> : null}
-          <CardDetail post={postDetail} fetchData={fetchData}/>
-          
+          <CardDetail post={ postDetail } fetchData={fetchDataDetail} />
         </Grid>
       </Grid>
     </div>
