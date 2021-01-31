cf0b4e8b706cb384c930808af59e0ec04f3f5deb
Talha-35 <tlh.34.bd@gmail.com>
2021-01-31 21:34:48 +0100

    created PAgination

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,32 +2,77 @@ import React, { useEffect, useState , useContext } from "react";
 import Container from "@material-ui/core/Container";
 import CardList from "../components/CardList";
 import axios from "axios";
-import {AuthContext} from "../context/AuthContext"
+import {AuthContext} from "../context/AuthContext";
+import logo from '../assets/load.gif';
+import { makeStyles } from "@material-ui/core/styles";
+import PaginatPage from '../components/Pagination';
 
 const HomePage = () => {
-  // const [postData, setPostData] = useState([]);
+  const [postData, setPostData] = useState([]);
+  const [loading, setLoading] = useState([])
+  const [currentPage, setCurrentPage] = useState(2);
+  const [postsPerPage, setPostsPerPage] = useState(5);
   const {postList, setPostList, fetchDataList}=useContext(AuthContext)
 
-  // async function fetchData() {
-  //   try {
-  //     const results = await axios.get(
-  //       "https://blog-backend-ysf.herokuapp.com/list/"
-  //     );
-  //     setPostData(results?.data);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
+  
+  const useStyles = makeStyles((theme) => ({
+    styleLogo:{
+      justifyContent: "center",
+      marginTop:50
+    }
+  }));
 
-  // console.log(postList)
+  const classes = useStyles();
+  
+  async function fetchData() {
+    setLoading(true)
+    try {
+      const results = await axios.get(
+        "https://blog-backend-ysf.herokuapp.com/list/"
+      );
+      setPostData(results?.data);
+      setLoading(false)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   useEffect(() => {
-    fetchDataList();
-  }, []);
+    fetchData();
+  }, [postList]);
 
-  return (
+  // Get current posts
+
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = postData.slice(indexOfFirstPost, indexOfLastPost)
+
+    const paginate = (pageNumber) => {
+      setCurrentPage(pageNumber)
+    }
+
+
+    {if (loading) {
+      return (
+        <div className={classes.container} >
+        <img className={classes.styleLogo} src={logo} alt="loading..." />
+
+        </div>
+      )
+
+     
+    }
+    else 
+    return(
     <Container>
-      <CardList postData={postList} />
+      <PaginatPage postsPerPage={postsPerPage} 
+      totalPosts={postData.length} 
+      paging={paginate} />
+
+
+      <CardList postData={currentPosts} />
     </Container>
-  );
+    )
+    }
 };
 export default HomePage;
